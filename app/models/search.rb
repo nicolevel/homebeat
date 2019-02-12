cd9899b097b423ef4788281a4d3a186e1d9c7347
Nicole Velasco <nicolevelasco455@gmail.com>
2019-02-11 22:51:56 -0400

    filter outline more

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,6 +5,7 @@ class Search < ApplicationRecord
     items = items.where(["name LIKE ?", category]) if category.present?
     items = items.where(["price >= ?", min_price]) if min_price.present?
     items = items.where(["price <= ?", max_price]) if max_price.present?
+    items = items.where(["name LIKE ?", brand]) if brand.present?
 
     return items
   end
